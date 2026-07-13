e26d958929c89f2a95e57779e2fba183f794fc48
Dhanush Girish <mail.dhanushgirish@gmail.com>
2026-07-13 21:41:51 +0530

    stemming update

---
+++ sentiment_analysis/code.py
@@ -0,0 +1,17 @@
+import re
+import nltk
+from nltk.stem.porter import PorterStemmer
+from nltk.corpus import stopwords
+
+nltk.download('stopwords')
+
+stremmer = PorterStemmer()
+
+def stemming(content):
+    stemmed_content = re.sub('[^a-zA-Z]', ' ', content)
+    stemmed_content = stemmed_content.lower()
+    stemmed_content = stemmed_content.split()
+    stemmed_content = [stremmer.stem(word) for word in stemmed_content if not word in set(stopwords.words('english'))]
+    stemmed_content = ' '.join(stemmed_content)
+    return stemmed_content
+    
